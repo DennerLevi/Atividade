00bba67702f818e2808646df5ba4ab02683bf553
denne <denne@DENNI>
2024-09-03 00:25:27 -0300

    criação da controler

---
+++ FI.WebAtividadeEntrevista/Controllers/BeneficiarioController.cs
@@ -0,0 +1,85 @@
+﻿using FI.AtividadeEntrevista.BLL;
+using FI.AtividadeEntrevista.DML;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using WebAtividadeEntrevista.Models;
+
+namespace WebAtividadeEntrevista.Controllers
+{
+    public class BeneficiarioController : Controller
+    {
+        [HttpPost]
+        public JsonResult Incluir(BeneficiarioModel model)
+        {
+            BoBeneficiario bo = new BoBeneficiario();
+
+            if (!this.ModelState.IsValid)
+            {
+                List<string> erros = (from item in ModelState.Values
+                                      from error in item.Errors
+                                      select error.ErrorMessage).ToList();
+
+                Response.StatusCode = 400;
+                return Json(string.Join(System.Environment.NewLine, erros));
+            }
+            else
+            {
+                model.Id = bo.Incluir(new Beneficiario()
+                {
+                    CPF = model.CPF,
+                    Nome = model.Nome,
+                    IdCliente = model.IdCliente
+                });
+
+                return Json(new { success = true, message = "Beneficiário incluído com sucesso!", beneficiarioId = model.Id });
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Alterar(BeneficiarioModel model)
+        {
+            BoBeneficiario bo = new BoBeneficiario();
+
+            if (!this.ModelState.IsValid)
+            {
+                List<string> erros = (from item in ModelState.Values
+                                      from error in item.Errors
+                                      select error.ErrorMessage).ToList();
+
+                Response.StatusCode = 400;
+                return Json(string.Join(System.Environment.NewLine, erros));
+            }
+            else
+            {
+                bo.Alterar(new Beneficiario()
+                {
+                    Id = model.Id,
+                    CPF = model.CPF,
+                    Nome = model.Nome,
+                    IdCliente = model.IdCliente
+                });
+
+                return Json(new { success = true, message = "Beneficiário alterado com sucesso!" });
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Excluir(long id)
+        {
+            BoBeneficiario bo = new BoBeneficiario();
+            bo.Excluir(id);
+
+            return Json(new { success = true, message = "Beneficiário excluído com sucesso!" });
+        }
+
+        [HttpGet]
+        public JsonResult ObterPorCliente(long clienteId)
+        {
+            BoBeneficiario bo = new BoBeneficiario();
+            List<Beneficiario> beneficiarios = bo.ObterPorCliente(clienteId);
+
+            return Json(beneficiarios, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
