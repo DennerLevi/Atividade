3c021873243cec43559145b38b10a270f65d1576
denne <denne@DENNI>
2024-09-02 23:17:20 -0300

    CORREÇÕES

--- FI.WebAtividadeEntrevista/Scripts/Clientes/FI.AltClientes.js
+++ FI.WebAtividadeEntrevista/Scripts/Clientes/FI.AltClientes.js
@@ -1,7 +1,8 @@
 ﻿
 $(document).ready(function () {
     $('#CPF').mask('000.000.000-00', { reverse: true });
-
+    $('#CEP').mask('00000-000');
+    $('#Telefone').mask('(00) 00000-0000');
 
     if (obj) {
         $('#formCadastro #Nome').val(obj.Nome);
